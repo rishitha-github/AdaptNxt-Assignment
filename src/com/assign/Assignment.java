bbadb416bb5d62ed42c2fe38454f947a818cdba6
rishitha-github <rishitha.madaparthi@gmail.com>
2023-03-28 14:47:06 +0530

    upoad

--- src/com/assign/Assignment.java
+++ src/com/assign/Assignment.java
@@ -34,20 +34,22 @@ import org.jsoup.select.Elements;
 		            csvWriter.append("\n");
 
 		            Document doc = Jsoup.connect(url).get();
-		            Elements productCards = doc.select("div.cls");
+		            Elements productCards = doc.select("div.row");
 
 		            for (Element card : productCards) {
-	                String name = card.select("div.search-product-name-wrap a").text();
-		                String price = card.select("div.h6").text();
+	                    String name = card.select("div.search-product-name-wrap a").text();
+		                String price = card.select("div.pricing-wrap").text();
 		                String itemNumber = card.select("div.LEffortFindNum").text();
 		                String modelNumber = card.select("div.mb-2").text();
 		                String category = card.select("div.category").text();
 		                String description = card.select("div.js-skuBullets.body-xxs.mb-2.pb-1").text();
-
+		                
+                       System.out.printf(name,price,itemNumber,modelNumber,category,description);
 		                String[] productDetails = {name, price, itemNumber, modelNumber, category, description};
 		                csvWriter.append(String.join(",", productDetails));
 		                csvWriter.append("\n");
 		            }
+		            
 		            csvWriter.close();
 		            System.out.println("Product details saved to " + csvFilePath);
 		        } catch (IOException e) {
