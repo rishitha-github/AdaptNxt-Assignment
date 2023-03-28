48efe906c17609230345c00c8d314e3fc2cf6926
rishitha-github <rishitha.madaparthi@gmail.com>
2023-03-28 13:11:18 +0530

    upoad

---
+++ src/com/assign/Assignment.java
@@ -0,0 +1,60 @@
+package com.assign;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+//Product Name 
+//
+//Product Price 
+//
+//Item Number/ SKU/ Product Code 
+//
+//Model Number 
+//
+//Product Category 
+//
+//Product Description 
+//
+// 
+
+		public class Assignment {
+
+		    public static void main(String[] args) {
+		        String url = "https://www.quill.com/hanging-file-folders/cbl/4378.html ";
+		        String csvFilePath = "products.csv";
+		        String[] headers = {"Product Name", "Product Price", "Item Number", "Model Number", "Product Category", "Product Description"};
+		        try {
+		            FileWriter csvWriter = new FileWriter(csvFilePath);
+		            csvWriter.append(String.join(",", headers));
+		            csvWriter.append("\n");
+
+		            Document doc = Jsoup.connect(url).get();
+		            Elements productCards = doc.select("div.cls");
+
+		            for (Element card : productCards) {
+	                String name = card.select("div.search-product-name-wrap a").text();
+		                String price = card.select("div.h6").text();
+		                String itemNumber = card.select("div.LEffortFindNum").text();
+		                String modelNumber = card.select("div.mb-2").text();
+		                String category = card.select("div.category").text();
+		                String description = card.select("div.js-skuBullets.body-xxs.mb-2.pb-1").text();
+
+		                String[] productDetails = {name, price, itemNumber, modelNumber, category, description};
+		                csvWriter.append(String.join(",", productDetails));
+		                csvWriter.append("\n");
+		            }
+		            csvWriter.close();
+		            System.out.println("Product details saved to " + csvFilePath);
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		}
+		
+
+
